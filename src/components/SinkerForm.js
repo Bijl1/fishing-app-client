b608bb571aea3058e6339c0afd0ee60a3ef9fd33
Jonathan Bijl <sannoayo@gmail.com>
2023-08-25 18:44:59 -0400

    sinkers redirect added

--- src/components/SinkerForm.js
+++ src/components/SinkerForm.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { createSinker } from '../api/api';
 
 const SinkerForm = () => {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     type: '',
     shape: '',
@@ -19,7 +21,8 @@ const SinkerForm = () => {
       const newlyCreatedSinker = await createSinker(formData);
       console.log({newlyCreatedSinker});
 
-      if(!!newlyCreatedSinker._id) {
+      if (newlyCreatedSinker._id) {
+        navigate('/sinkers');
       }
     } catch (error) {
       console.error('Error submitting form:', error);
@@ -28,7 +31,7 @@ const SinkerForm = () => {
 
   return (
     <div>
-      <h2>Add Sinker</h2> 
+      <h2>Add Sinker</h2>
       <form onSubmit={handleSubmit}>
         <label>Type:</label>
         <input type="text" name="type" value={formData.type} onChange={handleChange} />
